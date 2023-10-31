b91a4eb904e9b9a3df8a819dcf2e29163c95b57a
Tu <tuanhung@cs300u-7b.acad.fullerton.edu>
2023-10-30 20:19:59 -0700

    Lab 8 part 1

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// HungTu
+// TUANHUNG@csu.fullerton.edu
+// @HungTu555
+// Partners: @CSUFVincent
 
 #include <iostream>
 #include <string>
@@ -6,18 +9,18 @@
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
+  if (arguments.size() != 4) {
+    std::cout << "error: you must supply three arguments\n";
+    return 1;
+  }
+  std::string protein{arguments.at(1)};
+  std::string bread{arguments.at(2)};
+  std::string condiment{arguments.at(3)};
 
-  // TODO: Validate that the number of arguments is correct.
-  // If not, print an error message and return a non-zero value.
-
-  // TODO: Declare three std::string variables to hold the
-  // protein, bread, and condiment input.
-  // Initialize each variable with an element of the arguments vector
-  // declared above.
-
-  // TODO: Use cout to print output following the pattern
-  // Your order: A *PROTEIN* sandwich on *BREAD* with *CONDIMENT*.
-  // on its own line.
+  std::cout << "Your order: ";
+  std::cout << " A " << protein;
+  std::cout << " sandwich on " << bread;
+  std::cout << " bread with " << condiment;
 
   return 0;
 }
